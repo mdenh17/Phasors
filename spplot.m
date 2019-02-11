b1b313dd3364fd772d5824334d24a356ad4e628c
mdenh17 <32374546+mdenh17@users.noreply.github.com>
2019-02-11 20:09:35 +0100

    Add files via upload

--- spplot.m
+++ spplot.m
@@ -56,9 +56,9 @@ if nargin == 1
     end
     
 elseif nargin >= 2
- 
-   
-   clrs = {[0 0.4470 0.7410], 	[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560],[0.4660 0.6740 0.1880],[0.3010 0.7450 0.9330],  	[0.6350 0.0780 0.1840]};
+    
+    
+    clrs = {[0 0.4470 0.7410], 	[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560],[0.4660 0.6740 0.1880],[0.3010 0.7450 0.9330],  	[0.6350 0.0780 0.1840]};
     
     
     
@@ -85,7 +85,7 @@ elseif nargin >= 2
             elseif Vinkel < 0
                 pre='X_C:' ;
             else
-                  msg = 'Error occurred.';
+                msg = 'Error occurred.';
                 error(msg)
             end
         elseif contains(inputname(m),'S','IgnoreCase',true)
@@ -109,8 +109,9 @@ elseif nargin >= 2
             enhed = '\Omega';
             pre='Z:';
         else
-              msg = 'Error occurred.';
-            error(msg);
+            Scale = 1;
+            enhed = ' ';
+            pre=inputname(m);
         end
         
         
@@ -159,8 +160,16 @@ elseif nargin >= 2
             set(findall(gcf, 'String', '90'),'String', ' ' );
             
             
-            for u=0:1:abs(maxval)*1.5
-                set(findall(gcf, 'String', ['  ' num2str(u)]),'String', '  ');
+            for y=1:1:5
+                if y==5
+                    for u=1:1:(abs(maxval)/5)*(y*1.25)
+                        set(findall(gcf, 'String', ['  ' num2str(u)]),'String', '  ');
+                    end
+                else
+                    for u=1:1:(abs(maxval)/5)*(y)
+                        set(findall(gcf, 'String', ['  ' num2str(u)]),'String', '  ');
+                    end
+                end
             end
         end
         
@@ -186,7 +195,7 @@ elseif nargin >= 2
             text(real(plot)+real(plot)/8,imag(plot)+imag(plot)/8,[pre ' ' num2str(abs(Laengde),'%100.2f') ' ' enhed ' {\angle} ' num2str(Vinkel,'%100.2f') '^{\circ}' ])
             
         else
-              msg = 'Error occurred.';
+            msg = 'Error occurred.';
             error(msg)
             
             
@@ -198,7 +207,7 @@ elseif nargin >= 2
     end
     
 else
-      msg = 'Error occurred.';
+    msg = 'Error occurred.';
     error(msg)
     
     
