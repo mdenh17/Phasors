3d78fea236c517270c4abbdf2c8c16dae10a525d
mdenh17 <32374546+mdenh17@users.noreply.github.com>
2019-02-21 15:01:26 +0100

    Add files via upload

---
+++ Amp2RMS.m
@@ -0,0 +1,11 @@
+function [ sum ] = Amp2RMS( x )
+
+A=x(1)./sqrt(2);
+
+
+sum=[A x(2)];
+
+
+
+end
+
---
+++ RMS2Amp.m
@@ -0,0 +1,10 @@
+function [ sum ] = RMS2Amp( x )
+
+A=x(1).*sqrt(2);
+
+
+sum=[A x(2)];
+
+
+
+end
